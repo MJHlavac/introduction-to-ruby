56e7057366ac5a3b00c8773af2acd5eb94fcb327
Marc Hlavac <mhlava15@gmail.com>
2023-04-05 23:48:19 +0000

    conditionals problems

---
+++ ruby_basics/weather2.rb
@@ -0,0 +1,7 @@
+# Write an unless statement that prints "The clouds are blocking the sun!" unless sun equals 'visible'.
+
+sun = ['visible', 'hidden'].sample
+
+puts "The clouds are blockiing the sun!" unless sun == 'visible'
+
+#unless is the opposite of if. Great job. 
